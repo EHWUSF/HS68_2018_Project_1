683f425f9bfa54396de268974b525787d6d8fcea
douglas-yao <doug.yao@gmail.com>
2018-07-18 12:48:54 -0700

    Initialize python script to implement issue #4

---
+++ hs628_tools/random-forest.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+class randomforest:
+    def __init__(self, array):
+        self.array = array
+
+    def rf(self, array):
+
+
+
+
+
+
+
+### MAIN ###
+ __name__ == '__main__':
+
+data = pd.read_csv('data.csv')
+
