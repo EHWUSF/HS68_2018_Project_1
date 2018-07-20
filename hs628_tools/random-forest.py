f7cda94bc555f790f57dc8df2b5bb98d3a518250
douglas-yao <doug.yao@gmail.com>
2018-07-19 19:01:44 -0700

    Create initial randomforest function

--- hs628_tools/random-forest.py
+++ hs628_tools/random-forest.py
@@ -4,25 +4,24 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
 from sklearn import preprocessing
 
-### Function to wrap sklearn's randomforestclassifier and prediction model and
-### return a tuple: predictions of test data, ranked scores of features.
-def rf(data, features, target):
-    features['train'] = np.random.uniform(0, 1, len(data)) <= 0.75
-    train_features, test_features = features[features['train']==True], features[features['train']==False]
-    train_target, test_target = target[target['train']==True], target[target['train']==False]
+### Let the user decide between randomforest classifier or regressor. Return tuple of predictions and scores.
+def rf(features, target, model=classifier):
+    if model == 'classifier':
+        features['train'] = np.random.uniform(0, 1, len(data)) <= 0.75
+        train_features, test_features = features[features['train']==True], features[features['train']==False]
+        train_target, test_target = target[target['train']==True], target[target['train']==False]
 
-    clf = RandomForestClassifier(n_jobs=2, random_state=0)
-    clf.fit(train_features, train_target)
-    clf.predict(test_features)
+        clf = RandomForestClassifier(n_jobs=2, random_state=0)
+        clf.fit(train_features, train_target)
+        clf.predict(test_features)
 
-    # Fix this indexing, does it even work?!
-    predictions = test_features[clf.predict(test_features)]
-    scores = list(zip(train_features, clf.feature_importances_))
+        # Fix this indexing, does it even work?!
+        predictions = test_features[clf.predict(test_features)]
+        scores = list(zip(train_features, clf.feature_importances_))
 
-
-    return predictions, scores
-
-
+        return predictions, scores
+    elif model == 'regressor':
+        pass
 
 if __name__ == '__main__':
 
