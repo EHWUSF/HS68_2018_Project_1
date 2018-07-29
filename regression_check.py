be797df6a68b0546a5aba25746228d1784d94e72
rohitchadaram <rohitchadaram@gmail.com>
2018-07-29 04:19:26 -0700

    Finished issue #12 committing the final code with comments/doc-strings

--- regression_check.py
+++ regression_check.py
@@ -1,35 +1,75 @@
 # coding: utf-8
 
-
 import numpy as np
 import pandas as pd
 import sklearn as sk
 from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
 # %config IPCompleter.greedy=True
+# %matplotlib inline
 
 
-# ************************ Part 1 - Data Visualization to check Linear regression assumptions ******************
+#   ****** Data Visualization to check Linear regression assumptions and calculating new regression metric ******
 
 class RegCheck():
-    def __init__(self):
-        self.data = None
-        self.target = None
-        self.model = None
-        ## degrees of freedom population dep. variable variance
-        self._dft = None
-        ## degrees of freedom population error variance
-        self._dfe = None
+    """
+        This Class is created for checking Linear Regression assumptions and Evaluating the Model
+        The following functions are defined in this class :
 
-    def check_linearity_residual_homoscedasticity_independence_plt(self, X, y, linear):
+         func : check_linearity_residual_homoscedasticity_independence_plt checks to see if the
+                data can fit to a linear model/non-linear model. Also check the homoscedasticity
+                (check for the spread of data) of the residuals/error terms and also check for their
+                independence. We diagnose these through a Residuals vs Predicted values plot.If there
+                is no clear pattern and data symmetrically distributed it can be said to satisfy the
+                assumptions.
+
+         func : check_residuals_normality_plt checks to see the normality of the error terms through
+                a density plot of the residuals. If the graph looks like gaussian normal curve it can
+                be said that the residuals satisfy the assumption of normality.
+
+         func : check_predictors_independence_plt checks for the independence of each predictor by
+                plotting a heatmap of the correlations of each predictor with one and another. The
+                heatmap would clearly indicate if there are multicolinear predictors.
+
+         func : regression_metric builds a metric for evaluating the model. It is a harmonic mean of
+                the R-Squared value and the F-Statistic taking into consideration the probability of
+                significance of the F-statistic being significant than having an only intercept model.
+    """
+
+    def __init__(self):
+        # Instantiating the parameters used in the methods below :PredictorColumnnames,R2,
+        # F-stat,F-statpval,ResidualSumofSquares,RegressionMetric
+        self.colnames = None
+        self.rsquared = 0
+        self.fstatistic = 0
+        self.fstat_pval = 0
+        self.resid_sumsq = 0
+        self.regmetric = 0
+
+    def check_linearity_residual_homoscedasticity_independence_plt(self, X, y, model):
+        """
+        Checks to see if the data can fit to a linear model/non-linear model,checks the homoscedasticity
+         of the residuals/error terms and also checks for their independence.
+            -----------
+            Param:
+                X: 2-D numpy array of the predictors
+                y: 1-D numpy array of the response variables
+                linear : Linear regression model fitted to X(predictors) and y(response)
+            -----------
+            Return:
+                Doesn't return anything, just shows the plot object built
+        """
+
         # Plot of Residuals vs predicted values
         fig, axes = plt.subplots(1, 1, sharex=False, sharey=False)
         fig.suptitle('[Residual Plots]')
         fig.set_size_inches(12, 5)
-        axes.plot(linear.predict(X), y - linear.predict(X), 'bo--')
+        # plotting the predicted values from the linear model vs the error values
+        axes.plot(model.predict(X), y - model.predict(X), 'bo--')
         axes.axhline(y=0, color='k')
         axes.grid()
         axes.set_title('Linear/Non-Linear')
@@ -37,38 +77,107 @@ class RegCheck():
         axes.set_ylabel('residuals')
         plt.show()
 
-    def check_residuals_normality_plt(self, X, y, linear):
-        residuals = y - linear.predict(X)
+    def check_residuals_normality_plt(self, X, y, model):
+        """
+        Checks to see the normality of the error terms through a density plot of the residuals
+            -----------
+            Param:
+                X: 2-D numpy array of the predictors
+                y: 1-D numpy array of the response variables
+                linear : Linear regression model fitted to X(predictors) and y(response)
+            -----------
+            Return:
+                Doesn't return anything, just shows the plot object built
+        """
+        residuals = y - model.predict(X)
+
+        # Density plot of the residuals
         sns.distplot(residuals)
         plt.xlabel("Residuals")
         plt.title('Residual Normality Check')
         plt.show()
 
-    def check_predictors_independence_plt(self, X, y, linear, names):
-        data_df = pd.DataFrame(X, columns=names)
+    def check_predictors_independence_plt(self, X, y, colnames):
+        """
+        Checks to see the independence of the predictors by plotting a heatmap of the correlations
+            -----------
+            Param:
+                X: 2-D numpy array of the predictors
+                y: 1-D numpy array of the response variables
+                colnames : Column names of the predictors
+            -----------
+            Return:
+                Doesn't return anything, just shows the plot object built
+        """
+        self.colnames = colnames
+        data_df = pd.DataFrame(X, columns=self.colnames)
+
+        # Heatmap of the correlation matrix
         sns.heatmap(data_df.corr(), cmap='binary', annot=True)
         plt.suptitle('Heatmap of Correlations')
         plt.show()
 
+    def regression_metric(self, X, y):
+        """
+        Calculates a new regression_metric for evaluating the model. R-Squared,F-Statistic,
+        Residual Standard error are used from the summary of the model to calculate this metric.
+        Intuitively The RSE represents the average distance of the observed data from the model
+        thus the lower the RSE the better fit and F-value expresses how much of the model has improved
+        (compared to the mean) given the inaccuracy of the model and R-squared.R2 expresses how much
+        of the total variation in the data can be explained by the mode(the regression line).
+
+        Thus a harmonic between RSE/F-statistic and R2 is computed as a new metric.
+        It ranges from 0 - 1. 0 being the worst and 1 being the highest or the best fit.
+        Disclaimer : This is a original metric thought out from my knowledge of Linear regression metrics
+        I have tried my best to think of all edge cases it might not fail, but is not foolproof.
+            -----------
+            Param:
+                X: 2-D numpy array of the predictors
+                y: 1-D numpy array of the response variables
+            -----------
+            Return:
+                Returns a regression metric that defines the overall fit.
+        """
+        X = sm.add_constant(X)
+        result = sm.OLS(y, X).fit()
+
+        # Metrics required to calculate new reg metric extracted from the result object
+        self.rsquared = result.rsquared
+        self.fstatistic = result.fvalue
+        self.fstat_pval = result.f_pvalue
+        self.resid_sumsq = (result.mse_resid) ** 2
+
+        if self.fstat_pval <= 0.05 or self.rsquared >= 0:
+            self.regmetric = (1 / ((1 / self.rsquared) + ((self.resid_sumsq ** 0.5) / self.fstatistic)))
+        else:
+            self.regmetric = 0
+
+        return self.regmetric
+
 
 if __name__ == '__main__':
-    # Call the check_assumptions function to print all the plots and check for linear regression assumptions
+    # Loading the sample data to build and test the model
     data_load = np.genfromtxt('data.csv', delimiter=",", dtype=float, names=True)
+    # Converting the structured array into a normal numpy array
     data = data_load.view(np.float64).reshape(data_load.shape + (-1,))
+    # Extract the Column names of the predictors
     names = list(data_load.dtype.names)
     predictor_names = names[:len(names) - 1]
 
+    # Splitting the predictors and response into 2 seperate numpy arrays to build and fit the model
     datax = data[:, :-1]  # a 2-d array of all predictor variables
     datay = data[:, -1]  # a 1-d array of response variable
 
+    # Fitting the LinearReg to the data
     lr = LinearRegression()
     lr.fit(datax, datay)
 
+    # Instantiating a reg object from the RegCheck class
     reg = RegCheck()
 
-    # reg.check_linearity_plt(datax,datay,lr)
-    # reg.check_linearity_residual_homoscedasticity_independence_plt(datax,datay,lr)
-    # reg.check_predictors_independence_plt(datax,datay,lr,predictor_names)
-
-
-# ****************** Part 2 - Calculate new metric to report the model ****************
+    # Calling all the methods to run the diagnostic on the data
+    reg.check_linearity_residual_homoscedasticity_independence_plt(datax, datay, lr)
+    reg.check_residuals_normality_plt(datax, datay, lr)
+    reg.check_predictors_independence_plt(datax, datay, predictor_names)
+    reg_metric = reg.regression_metric(datax, datay)
+    print("The new regression metric to evaluate the model is : " + str(reg_metric))
