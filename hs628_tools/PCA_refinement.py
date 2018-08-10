48851988cba907aa6c4caab44e65d3a229ea7310
choikwunyu <cyu32@dons.usfca.edu>
2018-08-09 19:42:55 -0700

    reduced reliance on pandas and modified initial correlation identification

--- hs628_tools/PCA_refinement.py
+++ hs628_tools/PCA_refinement.py
@@ -8,35 +8,49 @@ Created on Sat Jul 21 14:43:54 2018
 
 import numpy as np
 import pandas as pd
-from sklearn.cross_validation import cross_val_score, ShuffleSplit
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 #find out which variables are correlated
 #X is the independent variables, Y is the dependent variables, names is the variable names
-def correlated_variables (X, Y, names):
-    """"This function finds out which variables are correlated
+def correlated_variables (X, Y, threshold, method):
+    """This function finds out which variables are correlated
         Args: 
-            X: independent variables
-            Y: dependent variables
-            names: variable names
+            X: numpy array containing independent variables
+            Y: numpy array dependent variables
+            threshold: correlation coefficient where we decide if the variables are correlated or not
+            method: {‘pearson’, ‘kendall’, ‘spearman’}
+                pearson : standard correlation coefficient
+                kendall : Kendall Tau correlation coefficient
+                spearman : Spearman rank correlation
         Returned: 
-            scores: list of variable names and their correlation scores
-    """"
-    rf = RandomForestRegressor(n_estimators=20, max_depth=4)
-    scores = []
-    for i in range(X.shape[1]):
-        score = cross_val_score(rf, X[:, i:i+1], Y, scoring="r2",
-                              cv=ShuffleSplit(len(X), 3, .3))
-        scores.append((round(np.mean(score), 3), names[i]))
-    return (sorted(scores, reverse=True))
+            corr_variables: index of feature columns with correlation greater than threshold
+    """
+    matrix = np.concatenate((X,Y), axis = 1)
+    corr_matrix = np.absolute(np.corrcoef(matrix))
+    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
+    corr_variables = [column for column in upper.columns if any(upper[column] > threshold)]
+    return corr_variables
+
+def correlation_plot(X, Y):
+    """This function helps to visualize the correlations so we can make better decisions
+        Args: 
+            X: numpy array containing independent variables
+            Y: numpy array dependent variables
+        Returned: 
+            plot showing correlations
+    """
+    cm =np.corrcoef(np.concatenate((X,Y), axis = 1))
+    plt.imshow(cm,interpolation='nearest')
+    plt.colorbar()
+    plt.show()
 
 def specified_scaling (df, names):
-    """" This function scales specific variables in the dataframe
+    """ This function scales specific variables in the dataframe
          Args: 
             df: dataframe to be scaled
-            names: list of the variables which need scaling
+            names: list of the variables which need scaling, names can be the list returned from correlated_variables()
         Returned: 
             df: Dataframe with the specificed variables scaled
     """
@@ -45,13 +59,13 @@ def specified_scaling (df, names):
     return df
     
 def specified_PCA (df, names, num_components):
-    """" This function performs PCA on selected variables
+    """ This function performs PCA on selected variables
         Args: 
             df: dataframe which has the variables which need PCA performed
             names: list of the variables which need PCA
         Returned: 
             finalDf: dataframe with PCA performed on selected variables
-    """"
+    """
     pca = PCA(n_components= num_components)
     principalComponents = pca.fit_transform(df[names])
     principalDf = pd.DataFrame(data = principalComponents)
