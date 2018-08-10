f86b338ad9364d924469d290e1e3bebe2e7a1bce
douglas-yao <doug.yao@gmail.com>
2018-08-10 14:18:12 -0700

    Incorporate kwargs as arg for rf()

--- hs628_tools/random-forest.py
+++ hs628_tools/random-forest.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
-def rf(features, target, model='classifier'):
+def rf(features, target, model='classifier', **kwargs):
     """Function to combine sklearn's randomforestclassifier and randomforestregressor methods
 
         Args:
@@ -16,8 +16,13 @@ def rf(features, target, model='classifier'):
     """
     train_features, train_target, test_features, test_target = train_test_split(features, target, test_size=0.25, random_state=13)
     if model == 'classifier':
-        clf = RandomForestClassifier(n_estimators=10, criterion='gini', max_features='auto',
-                                     n_jobs=1, random_state=13, class_weight='balanced')
+        my_defaults = {'n_estimators': 10, 'criterion': 'gini', 'max_features': 'auto', 'n_jobs': 1,
+                       'random_state': 13, 'class_weight': 'balanced'}
+        for k, v in my_defaults.iteritems():
+            if k not in kwargs:
+                kwargs[k] = v
+
+        clf = RandomForestClassifier(**kwargs)
         clf.fit(train_features, train_target)
         clf.predict(test_features)
         predictions = clf.predict(test_target)
@@ -25,7 +30,12 @@ def rf(features, target, model='classifier'):
 
         return predictions, scores
     elif model == 'regressor':
-        reg = RandomForestRegressor(n_estimators=20, max_features='auto')
+        my_defaults = {'n_estimators': 20, 'max_features':'auto'}
+        for k, v in my_defaults.iteritems():
+            if k not in kwargs:
+                kwargs[k] = v
+
+        reg = RandomForestRegressor(**kwargs)
         reg.fit(train_features, train_target)
         reg.predict(test_features)
         predictions = reg.predict(test_target)
