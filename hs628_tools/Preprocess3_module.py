ae20d022f91430c3d3f69f4ae317362f979cb608
nitieaj <ijarhuidese@usfca.edu>
2018-08-16 03:56:03 -0700

    -Comment and corections implemented.  -Some of the functions & plots would  be updated with dynamic X-label   capabilities.

--- hs628_tools/Preprocess3_module.py
+++ hs628_tools/Preprocess3_module.py
@@ -1,4 +1,14 @@
+"""
+This package aids the user to display descriptive statistics of 
+specified column grouped by the class label
+Input: Column name ,class label name
+Returns: A graphical plot of specified descriptive statistics
+
+"""
 import matplotlib
+import re
+import os
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib
@@ -6,9 +16,7 @@ import matplotlib.pyplot as plt
 
 
 class vizsumstat:
-
-        def __init__ (self,csvPath,colname,col_catname):
-
+        def __init__(self, csvPath, colname, col_catname):
                 self.df = pd.read_csv(csvPath)
                 self.colname = colname
                 self.col_catname = col_catname
@@ -18,6 +26,11 @@ class vizsumstat:
 
                 
         def group_by(self,tpls,idx=1,merge=True):
+                """
+                This helper function builds a dictionary
+                input: Tuple of column element , class label values on same row index
+                Returns Dictionary of key(class element) and value(column element)
+                """
                 d = dict()
                 for tpl in tpls:
                         k = tpl[idx]
@@ -26,6 +39,11 @@ class vizsumstat:
                 return d
 
         def coltuple(self,df,col,typpe):
+                """
+                Helper function builds a tuple of elements
+                Input: Elements of column to be described,class label categorical values
+                Returns : A tuple of combined elements 
+                """
                 #tuplle=[]
                 for i in range(len(df[col])):
                         for k in range(len(df[typpe])):
@@ -38,26 +56,41 @@ class vizsumstat:
                         return newtuple
                 
         def summarystat(self):
+                """
+                Function generates specified descriptive summary statistics for the value elements grouped 
+                by the key corresponding categorical key
+                Input: Elements stored in the dictionary
+                Returns: Summary statistics plot and calculated numeric values
+                """
                 for i in range(len(self.colgroupbyclass)):
                        avg=np.mean(self.colgroupbyclass[i])
                        median=np.median(  self.colgroupbyclass[i])
                        stdev=np.std(  self.colgroupbyclass[i])
-                       plt.hist(  self.colgroupbyclass[i], normed=True, bins=20)
-                       plt.title('The "Normal" Distribution with Mean & St. Devs.')
-                       plt.xlabel("Variables"); plt.ylabel("Frequency")
+                       plt.hist(  self.colgroupbyclass[i], density=True, bins=20)
+                       plt.title('Descriptive statistics: \n\t')
+                       plt.xlabel("Described variable"); plt.ylabel("Frequency")
                        plt.grid(True)
                        plt.rc('grid', linestyle="dashed", color='grey')
+                       plt.tight_layout()
                        plt.show()
                        #print(avg,median,stdev)
                        print(i,"Mean = {0}".format(avg),"Median = {0}".format(median),"stdev = {0}".format(stdev))
                 return avg,median,stdev
 
 def main():
-        col=(input("input col name"))
+        myfile=input("Input path to your file:     e.g C:/Desktop/650/project data/parkinsons_train_set.csv : _")
+        f=open(myfile,'r')
+        col=input("input col name")
         col_catname=(input("inputcategorical col name: "))
-        path  = "C:/Users/Nitie/Desktop/health_informatics/MSHI_CLASSES/summer_2018/650/project data/parkinsons_train_set.csv"
-        viz=vizsumstat(path,col,col_catname)
+        #path  = "C:/Users/Nitie/Desktop/health_informatics/MSHI_CLASSES/summer_2018/650/project data/parkinsons_train_set.csv"
+        viz=vizsumstat(f,col,col_catname)
         viz.summarystat()
+        f.close()
 
         
 if __name__ == "__main__": main()
+
+
+
+
+
