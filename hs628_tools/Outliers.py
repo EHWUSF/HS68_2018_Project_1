e9e799c51d614ed64cdb625ef88b5bada3dbed04
Helen Han <hhan14@usfca.edu>
2018-08-18 10:16:20 +0900

    Debugged functions

--- hs628_tools/Outliers.py
+++ hs628_tools/Outliers.py
@@ -26,29 +26,14 @@ class Outliers:
         self.data = None
 
 
-    def clean(self, pattern):
-        pat1 = bool(re.match(pattern='\d{}-\d{}', string='123-4560'))
-        pat2 = bool(re.match(pattern='\$\d*\.\d{}', string='$123.45'))
-        pat3 = bool(re.match(pattern='[A-Z]\w*', string='Australia'))
-        if bool(pattern.match(pat1, pat2, pat3)):
-            return(NaN)
-        else:
-            pass
-
-        for i in range(len(self.data)):
-            self.data[i] = [elem for elem in self.data[i] if elem.isnumeric()]
-        self.data = np.array(self.data, dtype=np.float)  # convert to float
-        print(self.data)
-
-
-    def calc_outliers(self, my_arr, out_lim):
+    def calc_outliers(self, my_arr, outlier_limit=2):
         """
         Concept:
             Function for calculating samples out of scope of two standard deviation in data and returning those collection
             of samples as outliers
         Parameters:
             my_arr:
-            out_lim:
+            outlier_limit: the number of standard diviations outside of which data points considered outliers
         Actions:
             Get quartile values for outlier limits: out of two standard deviation
         References:
@@ -58,14 +43,12 @@ class Outliers:
               (pyISC: A Bayesian Anomaly Detection Framework for Python;
               Proceedings of the Thirtieth International Florida Artificial Intelligence Research Society Conference)
         """
-        my_arr = np.random.randn(20, 10)  # data array
-        out_lim = np.self.std*1.96  # set outlier limit
+        self.mean = my_arr.mean(axis=0)
+        self.std = my_arr.std(axis=0)
         outliers = np.ones((my_arr.shape[0],), dtype=np.bool)
-        self.mean = np.self.mean(my_arr, axis=0)
-        self.std = np.self.std(my_arr, axis=0, drop=1)
-        for i in range(my_arr.shape[1]):  # added for detect outliers by each column
+        for i in range(len(my_arr.shape)):  # added for detect outliers by each column
             col = my_arr[:, i]
-            outliers[outliers] &= np.abs((col[outliers] - self.mean[i]) / self.std[i]) < out_lim
+            outliers[outliers] &= np.abs((col[outliers] - self.mean[i]) / self.std[i]) < outlier_limit
         return my_arr[outliers]
 
 
@@ -83,10 +66,8 @@ if __name__ == '__main__':
     import doctest
     doctest.testmod()
     result = read_testdata()
-
-
-
-    #converted_data.astype("f4")
+    outlier_finder = Outliers()
+    outlier_finder.calc_outliers(result)
 
 
 
